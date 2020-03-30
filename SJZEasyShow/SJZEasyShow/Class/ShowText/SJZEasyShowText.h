e82f90b71209344340ef4be798e4de0823673416
jiazuo.shao <jiazuo.shao@ikang.com>
2020-03-30 18:36:43 +0800

    123

---
+++ SJZEasyShow/SJZEasyShow/Class/ShowText/SJZEasyShowText.h
@@ -0,0 +1,17 @@
+//
+//  SJZEasyShowText.h
+//  SJZEasyShow
+//
+//  Created by SJZ on 2020/3/30.
+//  Copyright © 2020 iKang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SJZEasyShowText : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SJZEasyShow/SJZEasyShow/Class/ShowText/SJZEasyShowText.m
@@ -0,0 +1,32 @@
+//
+//  SJZEasyShowText.m
+//  SJZEasyShow
+//
+//  Created by SJZ on 2020/3/30.
+//  Copyright © 2020 iKang. All rights reserved.
+//
+
+#import "SJZEasyShowText.h"
+
+@interface SJZEasyShowText ()
+
+@property (nonatomic, strong) UIView * backgroundView;
+@property (nonatomic, strong) UILabel * textLabel;
+@property (nonatomic, strong) UIView * contentView;
+@property (nonatomic, strong) UIImageView * showImageView;
+
+@end
+
+@implementation SJZEasyShowText
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if(self) {
+        
+    }
+    
+    return self;
+}
+
+
+@end
